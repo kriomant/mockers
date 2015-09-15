a6e41fc514cfdd66ced19297a501e5cd96fa6643
Mikhail Trishchenkov <kriomant@gmail.com>
2015-09-15 20:06:12 +0600

    Move utility types to library

---
+++ src/lib.rs
@@ -0,0 +1,217 @@
+#![feature(box_raw)]
+
+use std::marker::PhantomData;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub trait CheckCall {
+    fn check_call(self: Box<Self>, args: *const u8) -> *mut u8;
+    fn get_mock_id(&self) -> usize;
+    fn get_method_id(&self) -> usize;
+}
+
+#[must_use]
+pub struct CallMatch0<Res> {
+    mock_id: usize,
+    method_id: usize,
+
+    _phantom: PhantomData<Res>,
+}
+impl<Res> CallMatch0<Res> {
+    pub fn new(mock_id: usize, method_id: usize) -> Self {
+        CallMatch0 {
+            mock_id: mock_id,
+            method_id: method_id,
+            _phantom: PhantomData
+        }
+    }
+}
+
+#[must_use]
+pub struct Expectation0<Res> {
+    call_match: CallMatch0<Res>,
+    result: Res,
+}
+impl<Res> Expectation0<Res> {
+    fn check(self) -> Res { self.result }
+}
+impl<Res> CheckCall for Expectation0<Res> {
+    fn check_call(self: Box<Self>, _args: *const u8) -> *mut u8 {
+        //let args_tuple: &() = unsafe { std::mem::transmute(args) };
+        let result = self.check();
+        Box::into_raw(Box::new(result)) as *mut u8
+    }
+    fn get_mock_id(&self) -> usize { self.call_match.mock_id }
+    fn get_method_id(&self) -> usize { self.call_match.method_id }
+}
+impl<Res> CallMatch0<Res> {
+    pub fn and_return(self, result: Res) -> Expectation0<Res> {
+        Expectation0 { call_match: self, result: result }
+    }
+}
+
+#[must_use]
+pub struct CallMatch1<Arg0, Res> {
+    mock_id: usize,
+    method_id: usize,
+    arg0: MatchArg<Arg0>,
+
+    _phantom: PhantomData<Res>,
+}
+impl<Arg0, Res> CallMatch1<Arg0, Res> {
+    pub fn new(mock_id: usize, method_id: usize, arg0: MatchArg<Arg0>) -> Self {
+        CallMatch1 {
+            mock_id: mock_id,
+            method_id: method_id,
+            arg0: arg0,
+            _phantom: PhantomData
+        }
+    }
+}
+
+#[must_use]
+pub struct Expectation1<Arg0, Res> {
+    call_match: CallMatch1<Arg0, Res>,
+    result: Res,
+}
+impl<Arg0, Res> Expectation1<Arg0, Res> {
+    fn check(self, arg0: &Arg0) -> Res {
+        (*self.call_match.arg0)(arg0).unwrap();
+        self.result
+    }
+}
+impl<Arg0, Res> CheckCall for Expectation1<Arg0, Res> {
+    fn check_call(self: Box<Self>, args: *const u8) -> *mut u8 {
+        let args_tuple: &(Arg0,) = unsafe { std::mem::transmute(args) };
+        let result = self.check(&args_tuple.0);
+        Box::into_raw(Box::new(result)) as *mut u8
+    }
+    fn get_mock_id(&self) -> usize { self.call_match.mock_id }
+    fn get_method_id(&self) -> usize { self.call_match.method_id }
+}
+impl<Arg0, Res> CallMatch1<Arg0, Res> {
+    pub fn and_return(self, result: Res) -> Expectation1<Arg0, Res> {
+        Expectation1 { call_match: self, result: result }
+    }
+}
+
+#[must_use]
+pub struct CallMatch2<Arg0, Arg1, Res> {
+    mock_id: usize,
+    method_id: usize,
+    arg0: MatchArg<Arg0>,
+    arg1: MatchArg<Arg1>,
+
+    _phantom: PhantomData<Res>,
+}
+impl<Arg0, Arg1, Res> CallMatch2<Arg0, Arg1, Res> {
+    pub fn new(mock_id: usize, method_id: usize,
+               arg0: MatchArg<Arg0>, arg1: MatchArg<Arg1>) -> Self {
+        CallMatch2 {
+            mock_id: mock_id,
+            method_id: method_id,
+            arg0: arg0,
+            arg1: arg1,
+            _phantom: PhantomData
+        }
+    }
+}
+
+#[must_use]
+pub struct Expectation2<Arg0, Arg1, Res> {
+    call_match: CallMatch2<Arg0, Arg1, Res>,
+    result: Res,
+}
+impl <Arg0, Arg1, Res> Expectation2<Arg0, Arg1, Res> {
+    fn check(self, arg0: &Arg0, arg1: &Arg1) -> Res {
+        (*self.call_match.arg0)(arg0).unwrap();
+        (*self.call_match.arg1)(arg1).unwrap();
+        self.result
+    }
+}
+impl<Arg0, Arg1, Res> CheckCall for Expectation2<Arg0, Arg1, Res> {
+    fn check_call(self: Box<Self>, args: *const u8) -> *mut u8 {
+        let args_tuple: &(Arg0, Arg1) = unsafe { std::mem::transmute(args) };
+        let result = self.check(&args_tuple.0, &args_tuple.1);
+        Box::into_raw(Box::new(result)) as *mut u8
+    }
+    fn get_mock_id(&self) -> usize { self.call_match.mock_id }
+    fn get_method_id(&self) -> usize { self.call_match.method_id }
+}
+impl<Arg0, Arg1, Res> CallMatch2<Arg0, Arg1, Res> {
+    pub fn and_return(self, result: Res) -> Expectation2<Arg0, Arg1, Res> {
+        Expectation2 { call_match: self, result: result }
+    }
+}
+
+pub type MatchArg<T> = Box<Fn(&T) -> Result<(), String>>;
+
+pub trait IntoMatchArg<T> {
+    fn into_match_arg(self) -> MatchArg<T>;
+}
+
+impl<T> IntoMatchArg<T> for MatchArg<T> {
+    fn into_match_arg(self) -> Self { self }
+}
+
+impl<'a, T> IntoMatchArg<T> for T
+    where T: 'static + Eq + std::fmt::Debug {
+
+    fn into_match_arg(self) -> MatchArg<T> {
+        Box::new(move |value| {
+            if *value == self {
+                Ok(())
+            } else {
+                Err(format!("{:?} is not equal to {:?}", value, self))
+            }
+        })
+    }
+}
+
+pub trait Mock {
+    fn new(id: usize, scenario_int: Rc<RefCell<ScenarioInternals>>) -> Self;
+}
+
+pub struct ScenarioInternals {
+    events: Vec<Box<CheckCall>>,
+}
+
+pub struct Scenario {
+    internals: Rc<RefCell<ScenarioInternals>>,
+    next_mock_id: usize,
+}
+
+impl Scenario {
+    pub fn new() -> Self {
+        Scenario {
+            internals: Rc::new(RefCell::new(ScenarioInternals {
+                events: Vec::new(),
+            })),
+            next_mock_id: 0,
+        }
+    }
+
+    pub fn create_mock<T: Mock>(&mut self) -> T {
+        T::new(self.get_next_mock_id(), self.internals.clone())
+    }
+
+    fn get_next_mock_id(&mut self) -> usize {
+        let id = self.next_mock_id;
+        self.next_mock_id += 1;
+        id
+    }
+
+    pub fn expect<C: CheckCall + 'static>(&mut self, call: C) {
+        self.internals.borrow_mut().events.push(Box::new(call));
+    }
+}
+
+impl ScenarioInternals {
+    pub fn call(&mut self, mock_id: usize, method_id: usize, args_ptr: *const u8) -> *mut u8 {
+        let event = self.events.remove(0);
+        if event.get_mock_id() != mock_id || event.get_method_id() != method_id {
+            panic!("Unexpected event");
+        }
+        event.check_call(args_ptr)
+    }
+}
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,138 +1,12 @@
 #![feature(box_raw)]
 
-use std::marker::PhantomData;
+extern crate mockers;
+
 use std::rc::Rc;
 use std::cell::RefCell;
-
-trait CheckCall {
-    fn check_call(self: Box<Self>, args: *const u8) -> *mut u8;
-    fn get_mock_id(&self) -> usize;
-    fn get_method_id(&self) -> usize;
-}
-
-#[must_use]
-struct CallMatch0<Res> {
-    mock_id: usize,
-    method_id: usize,
-
-    _phantom: PhantomData<Res>,
-}
-#[must_use]
-struct Expectation0<Res> {
-    call_match: CallMatch0<Res>,
-    result: Res,
-}
-impl<Res> Expectation0<Res> {
-    fn check(self) -> Res { self.result }
-}
-impl<Res> CheckCall for Expectation0<Res> {
-    fn check_call(self: Box<Self>, args: *const u8) -> *mut u8 {
-        let args_tuple: &() = unsafe { std::mem::transmute(args) };
-        let result = self.check();
-        Box::into_raw(Box::new(result)) as *mut u8
-    }
-    fn get_mock_id(&self) -> usize { self.call_match.mock_id }
-    fn get_method_id(&self) -> usize { self.call_match.method_id }
-}
-impl<Res> CallMatch0<Res> {
-    fn and_return(self, result: Res) -> Expectation0<Res> {
-        Expectation0 { call_match: self, result: result }
-    }
-}
-
-#[must_use]
-struct CallMatch1<Arg0, Res> {
-    mock_id: usize,
-    method_id: usize,
-    arg0: MatchArg<Arg0>,
-
-    _phantom: PhantomData<Res>,
-}
-#[must_use]
-struct Expectation1<Arg0, Res> {
-    call_match: CallMatch1<Arg0, Res>,
-    result: Res,
-}
-impl<Arg0, Res> Expectation1<Arg0, Res> {
-    fn check(self, arg0: &Arg0) -> Res {
-        (*self.call_match.arg0)(arg0).unwrap();
-        self.result
-    }
-}
-impl<Arg0, Res> CheckCall for Expectation1<Arg0, Res> {
-    fn check_call(self: Box<Self>, args: *const u8) -> *mut u8 {
-        let args_tuple: &(Arg0,) = unsafe { std::mem::transmute(args) };
-        let result = self.check(&args_tuple.0);
-        Box::into_raw(Box::new(result)) as *mut u8
-    }
-    fn get_mock_id(&self) -> usize { self.call_match.mock_id }
-    fn get_method_id(&self) -> usize { self.call_match.method_id }
-}
-impl<Arg0, Res> CallMatch1<Arg0, Res> {
-    fn and_return(self, result: Res) -> Expectation1<Arg0, Res> {
-        Expectation1 { call_match: self, result: result }
-    }
-}
-
-#[must_use]
-struct CallMatch2<Arg0, Arg1, Res> {
-    mock_id: usize,
-    method_id: usize,
-    arg0: MatchArg<Arg0>,
-    arg1: MatchArg<Arg1>,
-
-    _phantom: PhantomData<Res>,
-}
-#[must_use]
-struct Expectation2<Arg0, Arg1, Res> {
-    call_match: CallMatch2<Arg0, Arg1, Res>,
-    result: Res,
-}
-impl <Arg0, Arg1, Res> Expectation2<Arg0, Arg1, Res> {
-    fn check(self, arg0: &Arg0, arg1: &Arg1) -> Res {
-        (*self.call_match.arg0)(arg0).unwrap();
-        (*self.call_match.arg1)(arg1).unwrap();
-        self.result
-    }
-}
-impl<Arg0, Arg1, Res> CheckCall for Expectation2<Arg0, Arg1, Res> {
-    fn check_call(self: Box<Self>, args: *const u8) -> *mut u8 {
-        let args_tuple: &(Arg0, Arg1) = unsafe { std::mem::transmute(args) };
-        let result = self.check(&args_tuple.0, &args_tuple.1);
-        Box::into_raw(Box::new(result)) as *mut u8
-    }
-    fn get_mock_id(&self) -> usize { self.call_match.mock_id }
-    fn get_method_id(&self) -> usize { self.call_match.method_id }
-}
-impl<Arg0, Arg1, Res> CallMatch2<Arg0, Arg1, Res> {
-    fn and_return(self, result: Res) -> Expectation2<Arg0, Arg1, Res> {
-        Expectation2 { call_match: self, result: result }
-    }
-}
-
-type MatchArg<T> = Box<Fn(&T) -> Result<(), String>>;
-
-trait IntoMatchArg<T> {
-    fn into_match_arg(self) -> MatchArg<T>;
-}
-
-impl<T> IntoMatchArg<T> for MatchArg<T> {
-    fn into_match_arg(self) -> Self { self }
-}
-
-impl<'a, T> IntoMatchArg<T> for T
-    where T: 'static + Eq + std::fmt::Debug {
-
-    fn into_match_arg(self) -> MatchArg<T> {
-        Box::new(move |value| {
-            if *value == self {
-                Ok(())
-            } else {
-                Err(format!("{:?} is not equal to {:?}", value, self))
-            }
-        })
-    }
-}
+use mockers::{Scenario, ScenarioInternals, Mock,
+              MatchArg, IntoMatchArg,
+              CallMatch0, CallMatch1};
 
 trait A {
     fn foo(&self);
@@ -145,14 +19,16 @@ struct AMock {
     mock_id: usize,
 }
 
-impl AMock {
+impl Mock for AMock {
     fn new(id: usize, scenario_int: Rc<RefCell<ScenarioInternals>>) -> Self {
         AMock {
             scenario: scenario_int,
             mock_id: id,
         }
     }
+}
 
+impl AMock {
     fn foo(&self) {
         let args = ();
         let args_ptr: *const u8 = unsafe { std::mem::transmute(&args) };
@@ -162,12 +38,7 @@ impl AMock {
     }
 
     fn foo_call(&self) -> CallMatch0<()> {
-        CallMatch0 {
-            mock_id: self.mock_id,
-            method_id: 0,
-
-            _phantom: PhantomData,
-        }
+        CallMatch0::<()>::new(self.mock_id, 0)
     }
 
     fn bar(&self, arg: u32) {
@@ -180,13 +51,7 @@ impl AMock {
 
     fn bar_call<Arg0Match>(&self, arg0: Arg0Match) -> CallMatch1<u32, ()>
             where Arg0Match: IntoMatchArg<u32> {
-        CallMatch1 {
-            mock_id: self.mock_id,
-            method_id: 1,
-            arg0: arg0.into_match_arg(),
-
-            _phantom: PhantomData,
-        }
+        CallMatch1::new(self.mock_id, 1, arg0.into_match_arg())
     }
 
     fn baz(&self) -> u32 {
@@ -198,58 +63,7 @@ impl AMock {
     }
 
     fn baz_call(&self) -> CallMatch0<u32> {
-        CallMatch0 {
-            mock_id: self.mock_id,
-            method_id: 2,
-
-            _phantom: PhantomData,
-        }
-    }
-}
-
-struct ScenarioInternals {
-    events: Vec<Box<CheckCall>>,
-}
-
-struct Scenario {
-    internals: Rc<RefCell<ScenarioInternals>>,
-    next_mock_id: usize,
-}
-
-impl Scenario {
-    pub fn new() -> Self {
-        Scenario {
-            internals: Rc::new(RefCell::new(ScenarioInternals {
-                events: Vec::new(),
-            })),
-            next_mock_id: 0,
-        }
-    }
-
-    pub fn create_mock(&mut self) -> AMock {
-        AMock::new(self.get_next_mock_id(), self.internals.clone())
-    }
-
-    fn get_next_mock_id(&mut self) -> usize {
-        let id = self.next_mock_id;
-        self.next_mock_id += 1;
-        id
-    }
-
-    pub fn expect<C: CheckCall + 'static>(&mut self, call: C) {
-        self.internals.borrow_mut().events.push(Box::new(call));
-    }
-}
-
-impl ScenarioInternals {
-    pub fn call(&mut self, mock_id: usize, method_id: usize, args_ptr: *const u8) -> *mut u8 {
-        use std::ops::Deref;
-
-        let event = self.events.remove(0);
-        if event.get_mock_id() != mock_id || event.get_method_id() != method_id {
-            panic!("Unexpected event");
-        }
-        event.check_call(args_ptr)
+        CallMatch0::<u32>::new(self.mock_id, 2)
     }
 }
 
@@ -257,7 +71,7 @@ impl ScenarioInternals {
 #[should_panic(expected="Unexpected event")]
 fn test_unit() {
     let mut scenario = Scenario::new();
-    let mock = scenario.create_mock();
+    let mock = scenario.create_mock::<AMock>();
     scenario.expect(mock.foo_call().and_return(()));
     mock.bar(2);
 }
@@ -265,7 +79,7 @@ fn test_unit() {
 #[test]
 fn test_return() {
     let mut scenario = Scenario::new();
-    let mock = scenario.create_mock();
+    let mock = scenario.create_mock::<AMock>();
     scenario.expect(mock.baz_call().and_return(2));
     assert_eq!(2, mock.baz());
 }
@@ -285,7 +99,7 @@ fn less_than<T: 'static + PartialOrd + std::fmt::Debug>(limit: T) -> MatchArg<T>
 #[should_panic(expected="4 is not less than 3")]
 fn test_arg_match_failure() {
     let mut scenario = Scenario::new();
-    let mock = scenario.create_mock();
+    let mock = scenario.create_mock::<AMock>();
     scenario.expect(mock.bar_call(less_than(3)).and_return(()));
     mock.bar(4);
 }
@@ -293,7 +107,9 @@ fn test_arg_match_failure() {
 #[test]
 fn test_arg_match_success() {
     let mut scenario = Scenario::new();
-    let mock = scenario.create_mock();
+    let mock = scenario.create_mock::<AMock>();
     scenario.expect(mock.bar_call(less_than(3)).and_return(()));
     mock.bar(2);
 }
+
+//mock![A];
