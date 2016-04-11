c6588c6485a925e457b3daffe8a1f3cf18bd5173
Mikhail Trishchenkov <kriomant@gmail.com>
2016-04-11 17:20:47 +0600

    Increase max number of mock method arguments to 4

--- src/lib.rs
+++ src/lib.rs
@@ -183,6 +183,142 @@ impl<Arg0, Arg1, Res> CallMatch2<Arg0, Arg1, Res> {
     }
 }
 
+
+#[must_use]
+pub struct CallMatch3<Arg0, Arg1, Arg2, Res> {
+    mock_id: usize,
+    method_name: &'static str,
+    arg0: Box<MatchArg<Arg0>>,
+    arg1: Box<MatchArg<Arg1>>,
+    arg2: Box<MatchArg<Arg2>>,
+
+    _phantom: PhantomData<Res>,
+}
+impl<Arg0, Arg1, Arg2, Res> CallMatch3<Arg0, Arg1, Arg2, Res> {
+    pub fn new(mock_id: usize, method_name: &'static str,
+               arg0: Box<MatchArg<Arg0>>,
+               arg1: Box<MatchArg<Arg1>>,
+               arg2: Box<MatchArg<Arg2>>) -> Self {
+        CallMatch3 {
+            mock_id: mock_id,
+            method_name: method_name,
+            arg0: arg0,
+            arg1: arg1,
+            arg2: arg2,
+            _phantom: PhantomData
+        }
+    }
+}
+
+
+#[must_use]
+pub struct Expectation3<Arg0, Arg1, Arg2, Res> {
+    call_match: CallMatch3<Arg0, Arg1, Arg2, Res>,
+    result: MockCallResult<Res>,
+}
+impl <Arg0, Arg1, Arg2, Res> Expectation3<Arg0, Arg1, Arg2, Res> {
+    fn check(self, arg0: &Arg0, arg1: &Arg1, arg2: &Arg2) -> Res {
+        self.call_match.arg0.matches(arg0).unwrap();
+        self.call_match.arg1.matches(arg1).unwrap();
+        self.call_match.arg2.matches(arg2).unwrap();
+        self.result.get()
+    }
+}
+impl<Arg0, Arg1, Arg2, Res> CheckCall for Expectation3<Arg0, Arg1, Arg2, Res> {
+    fn check_call(self: Box<Self>, args: *const u8) -> *mut u8 {
+        let args_tuple: &(Arg0, Arg1, Arg2) = unsafe { std::mem::transmute(args) };
+        let result = self.check(&args_tuple.0, &args_tuple.1, &args_tuple.2);
+        Box::into_raw(Box::new(result)) as *mut u8
+    }
+    fn get_mock_id(&self) -> usize { self.call_match.mock_id }
+    fn get_method_name(&self) -> &'static str { self.call_match.method_name }
+    fn describe(&self) -> String {
+        format!("{}({}, {}, {})", self.get_method_name(),
+                                  self.call_match.arg0.describe(),
+                                  self.call_match.arg1.describe(),
+                                  self.call_match.arg2.describe())
+    }
+}
+impl<Arg0, Arg1, Arg2, Res> CallMatch3<Arg0, Arg1, Arg2, Res> {
+    pub fn and_return(self, result: Res) -> Expectation3<Arg0, Arg1, Arg2, Res> {
+        Expectation3 { call_match: self, result: MockCallResult::Return(result) }
+    }
+
+    pub fn and_panic(self, msg: String) -> Expectation3<Arg0, Arg1, Arg2, Res> {
+        Expectation3 { call_match: self, result: MockCallResult::Panic(msg) }
+    }
+}
+
+#[must_use]
+pub struct CallMatch4<Arg0, Arg1, Arg2, Arg3, Res> {
+    mock_id: usize,
+    method_name: &'static str,
+    arg0: Box<MatchArg<Arg0>>,
+    arg1: Box<MatchArg<Arg1>>,
+    arg2: Box<MatchArg<Arg2>>,
+    arg3: Box<MatchArg<Arg3>>,
+
+    _phantom: PhantomData<Res>,
+}
+impl<Arg0, Arg1, Arg2, Arg3, Res> CallMatch4<Arg0, Arg1, Arg2, Arg3, Res> {
+    pub fn new(mock_id: usize, method_name: &'static str,
+               arg0: Box<MatchArg<Arg0>>,
+               arg1: Box<MatchArg<Arg1>>,
+               arg2: Box<MatchArg<Arg2>>,
+               arg3: Box<MatchArg<Arg3>>) -> Self {
+        CallMatch4 {
+            mock_id: mock_id,
+            method_name: method_name,
+            arg0: arg0,
+            arg1: arg1,
+            arg2: arg2,
+            arg3: arg3,
+            _phantom: PhantomData
+        }
+    }
+}
+
+#[must_use]
+pub struct Expectation4<Arg0, Arg1, Arg2, Arg3, Res> {
+    call_match: CallMatch4<Arg0, Arg1, Arg2, Arg3, Res>,
+    result: MockCallResult<Res>,
+}
+impl <Arg0, Arg1, Arg2, Arg3, Res> Expectation4<Arg0, Arg1, Arg2, Arg3, Res> {
+    fn check(self, arg0: &Arg0, arg1: &Arg1, arg2: &Arg2, arg3: &Arg3) -> Res {
+        self.call_match.arg0.matches(arg0).unwrap();
+        self.call_match.arg1.matches(arg1).unwrap();
+        self.call_match.arg2.matches(arg2).unwrap();
+        self.call_match.arg3.matches(arg3).unwrap();
+        self.result.get()
+    }
+}
+impl<Arg0, Arg1, Arg2, Arg3, Res> CheckCall for Expectation4<Arg0, Arg1, Arg2, Arg3, Res> {
+    fn check_call(self: Box<Self>, args: *const u8) -> *mut u8 {
+        let args_tuple: &(Arg0, Arg1, Arg2, Arg3) = unsafe { std::mem::transmute(args) };
+        let result = self.check(&args_tuple.0, &args_tuple.1, &args_tuple.2, &args_tuple.3);
+        Box::into_raw(Box::new(result)) as *mut u8
+    }
+    fn get_mock_id(&self) -> usize { self.call_match.mock_id }
+    fn get_method_name(&self) -> &'static str { self.call_match.method_name }
+    fn describe(&self) -> String {
+        format!("{}({}, {}, {}, {})", self.get_method_name(),
+                                      self.call_match.arg0.describe(),
+                                      self.call_match.arg1.describe(),
+                                      self.call_match.arg2.describe(),
+                                      self.call_match.arg3.describe())
+    }
+}
+impl<Arg0, Arg1, Arg2, Arg3, Res> CallMatch4<Arg0, Arg1, Arg2, Arg3, Res> {
+    pub fn and_return(self, result: Res) -> Expectation4<Arg0, Arg1, Arg2, Arg3, Res> {
+        Expectation4 { call_match: self, result: MockCallResult::Return(result) }
+    }
+
+    pub fn and_panic(self, msg: String) -> Expectation4<Arg0, Arg1, Arg2, Arg3, Res> {
+        Expectation4 { call_match: self, result: MockCallResult::Panic(msg) }
+    }
+}
+
+
 /// Argument matcher
 ///
 /// Basically it is predicate telling whether argument
