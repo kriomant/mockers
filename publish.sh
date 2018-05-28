8c5cf72618ff7515ef81a24ffdac84eb857e8d92
Mikhail Trishchenkov <kriomant@gmail.com>
2018-05-29 00:30:29 +0700

    Publish 0.11.0

--- publish.sh
+++ publish.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-(cd mockers_codegen && cargo publish)
-(cd mockers_macros && cargo publish)
-(cd mockers && cargo publish)
-(cd mockers_derive && cargo publish)
+(cd mockers && cargo publish --allow-dirty)
+(cd mockers_derive && cargo publish --allow-dirty)
 
