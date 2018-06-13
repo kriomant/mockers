bf8440c18f9a0dad5b291ef55da557164205065d
Mikhail Trishchenkov <kriomant@gmail.com>
2018-06-13 21:45:24 +0700

    Add test for type name conflicts

---
+++ mockers/tests/type_names.rs
@@ -0,0 +1,30 @@
+#![feature(proc_macro)]
+
+///! Test that generated code doesn't conflict with types defined
+///! in user code. The most often example is defining local `Result`
+///! and `Error` types.
+
+extern crate mockers;
+extern crate mockers_derive;
+
+use mockers_derive::derive_mock;
+
+use mockers::Scenario;
+use mockers::matchers::ANY;
+
+type Result<T> = std::result::Result<T, String>;
+
+#[derive_mock]
+pub trait A {
+    fn foo(&self, a: &u32) -> Result<u32>;
+}
+
+#[test]
+fn test_any_works_for_refs() {
+    let scenario = Scenario::new();
+    let mock = scenario.create_mock::<AMock>();
+
+    scenario.expect(mock.foo_call(ANY).and_return(Ok(23)));
+
+    assert_eq!(Ok(23), mock.foo(&3));
+}
