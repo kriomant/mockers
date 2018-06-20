336669eacd442f4282c3d1c8093b4a29255cfa1a
Mikhail Trishchenkov <kriomant@gmail.com>
2018-06-20 23:34:45 +0700

    Update examples in guide, fixes #30
    Guide references obsolete mockers_macros crate which is now
    replaced with mockers_derive.

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ set -e
 
 # `mockers` can be build by nightly Rust only.
 (echo mockers/nighly ; cd mockers && cargo +nightly build)
-# Tests use `mockers_macros` and thus are only runnable with nightly Rust.
+# Tests use `mockers_derive` and thus are only runnable with nightly Rust.
 (echo mockers/nightly/test ; cd mockers && cargo +nightly test)
 # `mockers_derive` uses nightly Rust features.
 (echo mockers_derive/nightly ; cd mockers_derive && cargo +nightly build)
