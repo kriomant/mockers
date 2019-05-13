0b246673895f5f22eb08bbc3a6ce7d7bd1806923
Mikhail Trishchenkov <kriomant@gmail.com>
2019-05-13 12:28:17 +0700

    Ensure mock can be moved and still controlled by handler

---
+++ mockers/tests/move.rs
@@ -0,0 +1,25 @@
+use mockers::Scenario;
+
+///! Test that mock may be moved and still be controlled
+use mockers_derive::mocked;
+
+#[mocked]
+pub trait A {
+    fn foo(&self);
+}
+
+struct Wrapper(pub Box<A>);
+impl Wrapper {
+    fn foo(&self) { self.0.foo() }
+}
+
+#[test]
+fn test() {
+    let scenario = Scenario::new();
+    let (mock, handle) = scenario.create_mock_for::<A>();
+
+    let wrapper = Wrapper(Box::new(mock));
+
+    scenario.expect(handle.foo().and_return(()));
+    wrapper.foo();
+}
